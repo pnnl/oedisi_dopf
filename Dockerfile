a318a68d98171a2a673a3ae506043e3c32ca8a86
rounak-meyur <rm5nz@virginia.edu>
2024-02-06 11:39:43 -0800

    post analysis plots

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.10.6-slim-bullseye
-# ARG SCENARIO
+ARG SCENARIO
 RUN apt-get update && apt-get install -y git ssh
 RUN mkdir -p /root/.ssh
 
@@ -14,5 +14,5 @@ RUN apt update && apt install -y \
 WORKDIR /simulation
 COPY . .
 RUN pip install -r requirements.txt
-# RUN oedisi build --component-dict scenario/$SCENARIO/components.json --system scenario/$SCENARIO/system.json --target-directory build
-# ENTRYPOINT ["oedisi", "run", "--runner", "build/system_runner.json"]
+RUN oedisi build --component-dict scenario/$SCENARIO/components.json --system scenario/$SCENARIO/system.json --target-directory build
+ENTRYPOINT ["oedisi", "run", "--runner", "build/system_runner.json"]
--- post_process/post_analysis.py
+++ post_process/post_analysis.py
@@ -1,7 +1,6 @@
 import os, sys
 import plotter
 
-
 def plot_result_case(
         casename, alg, 
         time = [30,60,90], coordsys="2D", 
@@ -10,7 +9,7 @@ def plot_result_case(
         ):
 
     # Directory location
-    directory = f"../outputs/{casename}"
+    directory = f"./outputs/{casename}"
     topology_filepath = os.path.join(directory, "topology.json")
     buscoord_filepath = os.path.join(directory, "BusCoords.dat")
     real_voltage_filepath = os.path.join(directory, "voltage_real.feather")
@@ -49,88 +48,17 @@ def plot_result_case(
         )
     return
 
-def plot_result_set(
-        casename1, casename2, 
-        alg1, alg2, 
-        time = [30, 60, 90], coordsys="2D", 
-        root = "150", seperator = "    ", annotate=True, 
-        ymin = 0.98, ymax = 1.06, 
-        ):
-    plot_result_case(
-        casename1, alg1, 
-        time=time, coordsys=coordsys, 
-        root = root, seperator = seperator, annotate=annotate, 
-        ymin = ymin, ymax = ymax, 
-        )
-    plot_result_case(
-        casename2, alg2, 
-        time=time, coordsys=coordsys, 
-        root = root, seperator = seperator, annotate=annotate, 
-        ymin = ymin, ymax = ymax, 
-        )
-    return
-
-def plot_result_comparison(
-        casename1, casename2, time, 
-        coordsys="2D", root = "150", 
-        ymin = 0.98, ymax = 1.06, 
-        seperator = "    ", annotate=True
-        ):
-    dir1 = f"../outputs/{casename1}"
-    topofile1 = os.path.join(dir1, "topology.json")
-    buscordfile1 = os.path.join(dir1, "BusCoords.dat")
-    realVfile1 = os.path.join(dir1, "voltage_real.feather")
-    imagVfile1 = os.path.join(dir1, "voltage_imag.feather")
-    
-    dir2 = f"../outputs/{casename2}"
-    topofile2 = os.path.join(dir2, "topology.json")
-    buscordfile2 = os.path.join(dir2, "BusCoords.dat")
-    realVfile2 = os.path.join(dir2, "voltage_real.feather")
-    imagVfile2 = os.path.join(dir2, "voltage_imag.feather")
-
-    fig_filename1 = os.path.join(dir1, f"vtree_peak_{casename1}.png")
-    fig_filename2 = os.path.join(dir1, f"network_peak_{casename1}.png")
-    
-    plotter.compare_vtree(
-        realVfile1, realVfile2, 
-        imagVfile1, imagVfile2, 
-        topofile1, topofile2, 
-        buscordfile1, buscordfile2, 
-        alg1="LinDistFlow", alg2="OMOO", 
-        sep = seperator, root_node = root,
-        coordsys = coordsys, time = time,
-        to_file = fig_filename1, show=False, do_return=False,
-        lw=2.5, ls='dashed', 
-        figsize=(40,18), annotate=annotate,
-        ymin = ymin, ymax = ymax, 
-        )
-    
-    plotter.compare_network(
-        realVfile1, realVfile2, 
-        imagVfile1, imagVfile2, 
-        topofile1, topofile2, 
-        buscordfile1, buscordfile2, 
-        alg1="LinDistFlow", alg2="OMOO", 
-        sep = seperator, root_node = root,
-        coordsys = coordsys, time = time,
-        to_file = fig_filename2, show=False, do_return=False,
-        vmin=1.0, vmax=1.05,
-        )
-
-    return
-
-
 if __name__ == "__main__":
     
     case = sys.argv[1]
+    alg = "LinDistFlow"
+    time = [30,60,90]
 
     if case == "ieee123":
         root = "150"
         seperator = "    "
         cordsys = "2D"
         annotate=True
-        case1 = "ieee123"
-        case2 = "omoo"
         ymin = 0.98
         ymax = 1.06 
 
@@ -139,8 +67,6 @@ if __name__ == "__main__":
         seperator = " "
         cordsys = "GEO"
         annotate=False
-        case1 = "small"
-        case2 = "omoo_small"
         ymin = 1.02
         ymax = 1.035
 
@@ -149,27 +75,16 @@ if __name__ == "__main__":
         seperator = " "
         cordsys = "GEO"
         annotate=False
-        case1 = "medium"
-        case2 = "omoo_medium"
         ymin = 1.01
         ymax = 1.05
     
     else:
         print("Unknown case")
         sys.exit(0)
-    
 
-    plot_result_set(
-        case1, case2, 
-        alg1="LinDistFlow", alg2="OMOO", 
-        time = [30, 60, 90], coordsys=cordsys, 
-        root = root, seperator = seperator, annotate=annotate,
-        ymin = ymin, ymax = ymax, 
-        )
-    
-    plot_result_comparison(
-        case1, case2, time=45, 
-        coordsys=cordsys, root = root, 
-        seperator = seperator, annotate=annotate,
-        ymin = ymin, ymax = ymax, 
-        )
+    plot_result_case(
+            case, alg, 
+            time=time, coordsys=cordsys, 
+            root = root, seperator = seperator, annotate=annotate, 
+            ymin = ymin, ymax = ymax, 
+            )
